74b5b57e7da415d63c65205c10bb57d8d7915aa4
Ben Hulan <benhulan@gmail.com>
2016-05-11 15:46:35 -0700

    in-class solution

--- scripts/app.js
+++ scripts/app.js
@@ -6,29 +6,56 @@ var template;
 
 $(document).on("ready", function() {
 
+    initMap();
+
     $quakesList = $.ajax({
         method: "GET",
         url: weekly_quakes_endpoint
-    });
-
-    $quakesList.done(function(data){
+    }).done(function(data){
 
        var earthquakes = data.features;
 
-        // var source = $("#quakes-template").html();
-        // template = Handlebars.compile(source);
+        var source = $("#quakes-template").html();
+        template = Handlebars.compile(source);
 
-        // var quakesTemplate = template({quakes: earthquakes});
-        for (var i = 0; i < earthquakes.length; i++){
-        var quakesTemplate = "<p>" + earthquakes[i].properties.title + "</p>" +
-        "<h3>LATITUDE: " + earthquakes[i].geometry.coordinates[0] + "</h3>";
+        var quakesTemplate = template({quakes: earthquakes});
+       
         $("#info").append(quakesTemplate);
-        }
-    });
 
-    $quakesList.fail(function(response){
+
+        earthquakes.forEach(function(quake){
+            var tempLat = quake.geometry.coordinates[1];
+            var tempLng = quake.geometry.coordinates[0];
+            new google.maps.Marker({
+                position: new google.maps.LatLng(tempLat, tempLng),
+                map: map,
+                title: quake.properties.title
+            });
+            
+        })
+
+    }).fail(function(response){
         console.log("Error: ", response);
     });
 });
+
+    
+    function initMap() {
+        var pos = {lat: 37.78, lng: -122.44};
+
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: pos,
+          zoom: 2
+        });
+
+        // var marker = new google.maps.Marker({
+        //     position: pos,
+        //     map: map,
+        //     title: "San Francisco"
+        // })
+      }
+
+
+
 
 
