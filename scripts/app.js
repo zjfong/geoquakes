5942d9bc1ab16251cc08959258d855cceba98547
Ben Hulan <benhulan@gmail.com>
2016-05-11 15:06:30 -0700

    adds jquery solution

--- scripts/app.js
+++ scripts/app.js
@@ -6,5 +6,29 @@ var template;
 
 $(document).on("ready", function() {
 
+    $quakesList = $.ajax({
+        method: "GET",
+        url: weekly_quakes_endpoint
+    });
 
+    $quakesList.done(function(data){
+
+       var earthquakes = data.features;
+
+        // var source = $("#quakes-template").html();
+        // template = Handlebars.compile(source);
+
+        // var quakesTemplate = template({quakes: earthquakes});
+        for (var i = 0; i < earthquakes.length; i++){
+        var quakesTemplate = "<p>" + earthquakes[i].properties.title + "</p>" +
+        "<h3>LATITUDE: " + earthquakes[i].geometry.coordinates[0] + "</h3>";
+        $("#info").append(quakesTemplate);
+        }
+    });
+
+    $quakesList.fail(function(response){
+        console.log("Error: ", response);
+    });
 });
+
+
