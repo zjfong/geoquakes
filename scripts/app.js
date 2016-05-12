d1fc60648a67cfa1c77a1b4ae730c6bed8008d25
Ben Hulan <benhulan@gmail.com>
2016-05-12 14:53:33 -0700

    OOP refactor example

--- scripts/app.js
+++ scripts/app.js
@@ -2,59 +2,67 @@
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 var $quakesList;
 var map;
-var template;
+// var template;
 
 $(document).on("ready", function() {
 
     initMap();
 
-    $.ajax({
+    $quakesList = $.ajax({
         method: "GET",
         url: weekly_quakes_endpoint
     })
     .done(function(data){
-        console.log("Title: ", data.features[0].properties.title);
-        console.log("Coordinates: Lat: ", data.features[0].geometry.coordinates[1],
-            "Coordinates: Lng: ", data.features[0].geometry.coordinates[0]);
-
-        // handlebars shit
-        var earthquakes = data.features;
-        var source = $("#quakes-template").html();
-        template = Handlebars.compile(source);
-        var quakesTemplate = template({quakes: earthquakes});
-        $quakesList = $("#info").append(quakesTemplate);
-
-        earthquakes.forEach(function(quake){
-            new google.maps.Marker({
-                position: new google.maps.LatLng(quake.geometry.coordinates[1], quake.geometry.coordinates[0]),
-                map: map,
-                title: quake.properties.title
-            })
-            
-        })
-
-
+        onSuccess(data);
     })
     .fail(function(response){
-        console.log("Error: ", response);
+    console.log("Error: ", response);
     });
 
 });
 
-function initMap(){
-    var pos = {lat: 37.77, lng: -122.45};
+    function onSuccess(data){
+       var earthquakes = data.features;
+       createMarkers(earthquakes);
+       compileHandlebarsTemplate(earthquakes, "#info", "#quakes-template");
+    }
 
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: pos,
-        zoom: 2
-    });
+    function createMarkers(locationArray){
+        locationArray.forEach(function(location){
+            var tempLat = location.geometry.coordinates[1];
+            var tempLng = location.geometry.coordinates[0];
+            new google.maps.Marker({
+                position: new google.maps.LatLng(tempLat, tempLng),
+                map: map,
+                title: location.properties.title
+            });
+            
+        })
+    }
 
-    var marker = new google.maps.Marker({
-        position: pos,
-        map: map,
-        title: "San Francisco"
-    })
-}
+    function compileHandlebarsTemplate(data, targetHtml, targetScript){
+        var source = $(targetScript).html();
+        template = Handlebars.compile(source);
+
+        var dataTemplate = template({quakes: data});
+
+        $(targetHtml).append(dataTemplate);
+    }
+
+    function initMap(){
+        var pos = {lat: 37.77, lng: -122.45};
+
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: pos,
+            zoom: 2
+        });
+
+        var marker = new google.maps.Marker({
+            position: pos,
+            map: map,
+            title: "San Francisco"
+        });
+    }
 
 
 
