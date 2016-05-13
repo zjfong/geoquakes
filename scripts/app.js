e135bffd2fc5a9a920221fd077e527a7ff04502f
Ben Hulan <benhulan@gmail.com>
2016-05-13 10:06:10 -0700

    adds in class work up to stop point

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,33 @@
 // define globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+var gaLocations = "scripts/ga3.json";
 var $quakesList;
 var map;
-// var template;
+var gaImage = "ga-image.png";
+
 
 $(document).on("ready", function() {
 
     initMap();
+
+    $.getJSON(gaLocations, function(json){
+        console.log(json);
+
+        var gaCampuses = json.location;
+        createMarkers(gaCampuses, gaImage);
+
+    });
 
     $quakesList = $.ajax({
         method: "GET",
         url: weekly_quakes_endpoint
     })
     .done(function(data){
-        onSuccess(data);
+       // console.log(data);
+       var earthquakes = data.features;
+       var quakeIcon = "earthquake.png";
+       createMarkers(earthquakes, quakeIcon);
+       compileHandlebarsTemplate(earthquakes, "#info", "#quakes-template");
     })
     .fail(function(response){
     console.log("Error: ", response);
@@ -21,21 +35,29 @@ $(document).on("ready", function() {
 
 });
 
-    function onSuccess(data){
-       var earthquakes = data.features;
-       createMarkers(earthquakes);
-       compileHandlebarsTemplate(earthquakes, "#info", "#quakes-template");
-    }
+    // OPTION 1: Write function mungeData()
 
-    function createMarkers(locationArray){
+
+    function createMarkers(locationArray, customIcon){
         locationArray.forEach(function(location){
             var tempLat = location.geometry.coordinates[1];
             var tempLng = location.geometry.coordinates[0];
-            new google.maps.Marker({
+            var tempContentString = "<p>" + location + "</p>";
+            
+            var tempInfowindow = new google.maps.InfoWindow({
+                content: tempContentString
+            });
+
+
+            var tempMarker = new google.maps.Marker({
                 position: new google.maps.LatLng(tempLat, tempLng),
                 map: map,
-                title: location.properties.title
+                title: "I am a pilgrim",
+                icon: customIcon
             });
+            tempMarker.addListener('click', function(){
+                tempInfowindow.open(map, tempMarker);
+            })
             
         })
     }
@@ -57,11 +79,12 @@ $(document).on("ready", function() {
             zoom: 2
         });
 
-        var marker = new google.maps.Marker({
-            position: pos,
-            map: map,
-            title: "San Francisco"
-        });
+        // var marker = new google.maps.Marker({
+        //     position: pos,
+        //     map: map,
+        //     title: "San Francisco",
+        //     icon: "ga-image.png"
+        // });
     }
 
 
